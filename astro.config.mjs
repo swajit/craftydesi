8f9f7e3b3716af293be295fd2af652a96eab9506
swajit <swajits@gmail.com>
2026-02-22 01:36:00 -0500

    Fix image and link paths for custom domain redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://swajit.github.io', // Placeholder - will need repo name if not a user site
-  base: '/craftydesi', // Placeholder repo name
+  site: 'https://craftydesi.ssatam.com',
+  base: '/',
   integrations: [mdx(), sitemap()],
   output: 'static'
 });
